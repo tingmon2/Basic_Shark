1c478b25ac65ced54d36d00d99cb4a27091dd47c
tingmon2 <128055745+tingmon2@users.noreply.github.com>
2023-07-22 17:06:22 -0600

    frame = header + payload
    break down frame finely

--- basic_shark.c
+++ basic_shark.c
@@ -144,21 +144,23 @@ void packet_handler(u_char* param, const struct pcap_pkthdr* header, const u_cha
 	
 	if (header->len < 14) // it can't be and shouldn't be happen
 		return;
-	ether_header* pEther = (ether_header*)pkt_data;
+	frame_data* pFrame = (frame_data*)pkt_data;
 	// print hex value MAC address
 	// https://en.wikipedia.org/wiki/EtherType
 	printf("src MAC: %02X-%02X-%02X-%02X-%02X-%02X -> dst MAC: %02X-%02X-%02X-%02X-%02X-%02X (type: %04X)\n",
-		pEther->src_mac[0], pEther->src_mac[1], pEther->src_mac[2], pEther->src_mac[3], 
-		pEther->src_mac[4], pEther->src_mac[5],
-		pEther->dst_mac[0], pEther->dst_mac[1], pEther->dst_mac[2], pEther->dst_mac[3], 
-		pEther->dst_mac[4], pEther->dst_mac[5],
-		pEther->type);
+		pFrame->src_mac[0], pFrame->src_mac[1], pFrame->src_mac[2], pFrame->src_mac[3], 
+		pFrame->src_mac[4], pFrame->src_mac[5],
+		pFrame->dst_mac[0], pFrame->dst_mac[1], pFrame->dst_mac[2], pFrame->dst_mac[3], 
+		pFrame->dst_mac[4], pFrame->dst_mac[5],
+		pFrame->type);
 
 	// print IP address
 	// https://en.wikipedia.org/wiki/Internet_Protocol_version_4#Packet_structure
 	// skip 64 bit of ipv4 header
 	char buf[INET_ADDRSTRLEN], buf6[INET6_ADDRSTRLEN];
-	printf("src IP: %s ->", inet_ntop(AF_INET, (IN_ADDR*)(pkt_data + sizeof(ether_header) + 12), buf, sizeof(buf)));
-	printf(" dst IP: %s\n\n", inet_ntop(AF_INET, (IN_ADDR*)(pkt_data + sizeof(ether_header) + 16), buf, sizeof(buf)));
-
+	//printf("src IP: %s ->", inet_ntop(AF_INET, (IN_ADDR*)(pkt_data + sizeof(ether_header) + 12), buf, sizeof(buf)));
+	//printf(" dst IP: %s\n\n", inet_ntop(AF_INET, (IN_ADDR*)(pkt_data + sizeof(ether_header) + 16), buf, sizeof(buf)));
+	printf("src IP: %d.%d.%d.%d -> dst IP: %d.%d.%d.%d\n\n", 
+		pFrame->src_ip[0], pFrame->src_ip[1], pFrame->src_ip[2], pFrame->src_ip[3],
+		pFrame->dst_ip[0], pFrame->dst_ip[1], pFrame->dst_ip[2], pFrame->dst_ip[3]);
 }
--- basic_shark.h
+++ basic_shark.h
@@ -4,7 +4,14 @@
 // why? size of the stuct must be equal as Ethernet Header size. Otherwise, packet info we be tangled
 #pragma pack(push, 1) 
 typedef struct payload {
-	unsigned char info[12];
+	unsigned char version_ihl[1];
+	unsigned char dscp_ecn[1];
+	unsigned char length[2];
+	unsigned char identification[2];
+	unsigned char flag_offset[2];
+	unsigned char ttl[1];
+	unsigned char protocol[1];
+	unsigned char checksum[2];
 	unsigned char src_ip[4];
 	unsigned char dst_ip[4];
 } payload;
@@ -15,7 +22,13 @@ typedef struct ether_header {
 	unsigned char dst_mac[6];
 	unsigned char src_mac[6];
 	unsigned short type; // ethernet header size 14 bytes
-	struct payload;
 } ether_header;
+#pragma pack(pop)
+
+#pragma pack(push, 1) 
+typedef struct frame_data {
+	struct ether_header;
+	struct payload;
+} frame_data;
 #pragma pack(pop)
 
